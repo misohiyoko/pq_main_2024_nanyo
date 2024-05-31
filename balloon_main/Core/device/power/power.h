64bf38ee75b8242a03f9402857ecdf5dacb49201
misohiyoko <misohiyoko@gmail.com>
2024-05-31 12:28:15 +0900

    change pow

--- balloon_main/Core/device/power/power.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by genta on 2024/05/19.
-//
-
-#ifndef BALLOON_MAIN_POWER_H
-#define BALLOON_MAIN_POWER_H
-
-#endif //BALLOON_MAIN_POWER_H
--- balloon_main/Core/device/power/powerSupply.c
+++ balloon_main/Core/device/power/powerSupply.c
@@ -2,4 +2,4 @@
 // Created by genta on 2024/05/19.
 //
 
-#include "power.h"
+#include "powerSupply.h"
